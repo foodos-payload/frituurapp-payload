823bd7153d91e332c4792775fbf8b18d23b035cb
JonasDH9999 <jonas@covonet.be>
2024-12-23 11:35:16 +0100

    fix:remove migrations from gitignore

---
+++ src/migrations/index.ts
@@ -0,0 +1,21 @@
+import * as migration_20241223_082756 from './20241223_082756';
+import * as migration_20241223_102117 from './20241223_102117';
+import * as migration_seed from './seed';
+
+export const migrations = [
+  {
+    up: migration_20241223_082756.up,
+    down: migration_20241223_082756.down,
+    name: '20241223_082756',
+  },
+  {
+    up: migration_20241223_102117.up,
+    down: migration_20241223_102117.down,
+    name: '20241223_102117',
+  },
+  {
+    up: migration_seed.up,
+    down: migration_seed.down,
+    name: 'seed'
+  },
+];
