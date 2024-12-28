bbead981004a0d28923e0efc130f82a12db952fe
JonasDH9999 <jonas@covonet.be>
2024-12-28 12:27:20 +0100

    fix: allow digital ocean domain in api responses

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,11 @@ import { withPayload } from '@payloadcms/next/withPayload'
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Your Next.js config here
+  images: {
+    domains: [
+      'payloadmedia.ams3.cdn.digitaloceanspaces.com', // Add your S3 CDN domain here
+    ],
+  },
 }
 
 export default withPayload(nextConfig)
