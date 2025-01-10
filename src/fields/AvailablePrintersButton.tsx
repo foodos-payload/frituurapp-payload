0312a2653926b59b49f92ca12137000d2ab74c52
Jelle Van Renterghem <van.renterghem.jelle@gmail.com>
2025-01-10 14:52:54 +0100

    fix: available printers button remote access

--- src/fields/AvailablePrintersButton.tsx
+++ src/fields/AvailablePrintersButton.tsx
@@ -2,10 +2,12 @@
 
 import React from "react";
 
+const zeroTierURL = process.env.ZNET_LOGIN_IP || 'localhost';
+
 const AvailablePrintersButton: React.FC = () => {
     const handleRedirect = () => {
         window.open(
-            "http://localhost:3001/organization/cm5pv3e3u0005pm017bct9yo7/8a1946d1315f6d5b/",
+            `http://${zeroTierURL}:3001/organization/cm5pv3e3u0005pm017bct9yo7/8a1946d1315f6d5b/`,
             "_blank"
         );
     };
