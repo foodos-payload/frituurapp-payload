da7c90148ccaf62a81c338de4b9807c761e7ac32
Jelle Van Renterghem <van.renterghem.jelle@gmail.com>
2025-01-12 03:54:24 +0100

    build: disable ssr for api-doc route

--- src/app/api-doc/page.tsx
+++ src/app/api-doc/page.tsx
@@ -1,9 +1,13 @@
-// File: src/app/api-doc/page.tsx
 "use client"
 
 import React from 'react'
-import SwaggerUI from 'swagger-ui-react'
+import dynamic from 'next/dynamic'
 import 'swagger-ui-react/swagger-ui.css'
+
+// Dynamically import swagger-ui-react with no SSR
+const SwaggerUI = dynamic(() => import('swagger-ui-react'), {
+    ssr: false,
+})
 
 export default function SwaggerDocPage() {
     return <SwaggerUI url="/api-doc/swagger.json" />
