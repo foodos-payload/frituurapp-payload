bb8fc7f85a7f334e468dae2d87f595e575978249
Jelle Van Renterghem <van.renterghem.jelle@gmail.com>
2025-01-16 00:38:27 +0100

    feat: hex picker component

--- src/app/(payload)/admin/importMap.js
+++ src/app/(payload)/admin/importMap.js
@@ -1,6 +1,7 @@
 import { default as default_62597bfa3c1050fb10d86f1e5e1e2f21 } from '@/fields/custom-field/collections-field.tsx'
 import { TenantFieldComponent as TenantFieldComponent_0e322269e5426a9b98ca88b6faa9d3d0 } from '@/fields/TenantField/components/Field'
 import { default as default_cc7c43cc23f37bd496482c00da0b8f2d } from '@/fields/ShopGeocodeButton'
+import { ColorPickerFieldComponent as ColorPickerFieldComponent_95f860801d0381344387ae1d8e2f4d24 } from '@innovixx/payload-color-picker-field/components'
 import { default as default_561f4ffc1b5843e389baccba35a5fdc0 } from '@/fields/PrinterPortalButton'
 import { default as default_03bfcb996d8cd81c29ed21106a8dad21 } from '@/fields/CustomerQRField'
 import { TenantSelectorRSC as TenantSelectorRSC_9d7720c4b50db35595dfefa592fabd33 } from '@/components/TenantSelector'
@@ -9,6 +10,7 @@ export const importMap = {
   "@/fields/custom-field/collections-field.tsx#default": default_62597bfa3c1050fb10d86f1e5e1e2f21,
   "@/fields/TenantField/components/Field#TenantFieldComponent": TenantFieldComponent_0e322269e5426a9b98ca88b6faa9d3d0,
   "@/fields/ShopGeocodeButton#default": default_cc7c43cc23f37bd496482c00da0b8f2d,
+  "@innovixx/payload-color-picker-field/components#ColorPickerFieldComponent": ColorPickerFieldComponent_95f860801d0381344387ae1d8e2f4d24,
   "@/fields/PrinterPortalButton#default": default_561f4ffc1b5843e389baccba35a5fdc0,
   "@/fields/CustomerQRField#default": default_03bfcb996d8cd81c29ed21106a8dad21,
   "@/components/TenantSelector#TenantSelectorRSC": TenantSelectorRSC_9d7720c4b50db35595dfefa592fabd33
